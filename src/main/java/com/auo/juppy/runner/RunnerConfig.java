2b4de18eba0c2dc95c0c2b0690661395dd1e3027
auo <andreas.lindgren@outlook.com>
2020-10-20 14:20:56 +0200

    Set min and max values for interval and timeout
    - interval: 1 min - 24 hours
    - timout: 50 ms - 15 sec
    - Closes #5

--- src/main/java/com/auo/juppy/runner/RunnerConfig.java
+++ src/main/java/com/auo/juppy/runner/RunnerConfig.java
@@ -2,10 +2,18 @@ package com.auo.juppy.runner;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.time.Duration;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class RunnerConfig {
+
+    protected static final long MIN_INTERVAL = Duration.ofMinutes(1).toMillis();
+    protected static final long MAX_INTERVAL = Duration.ofDays(1).toMillis();
+
+    protected static final long MIN_TIMEOUT = Duration.ofMillis(50).toMillis();
+    protected static final long MAX_TIMEOUT = Duration.ofSeconds(15).toMillis();
+
     private static final Pattern SCHEME = Pattern.compile("http|https");
 
     public long created;
@@ -51,12 +59,20 @@ public class RunnerConfig {
             throw new IllegalArgumentException("Invalid URI, host is 0 chars");
         }
 
-        if (timeout <= 0) {
-            throw new IllegalArgumentException("Timeout is too small");
+        if (timeout < MIN_TIMEOUT) {
+            throw new IllegalArgumentException("Timeout is too small, min: " + MIN_TIMEOUT);
         }
 
-        if (interval <= 0) {
-            throw new IllegalArgumentException("Interval is too small");
+        if (timeout > MAX_TIMEOUT) {
+            throw new IllegalArgumentException("Timeout is too large, max: " + MAX_TIMEOUT);
+        }
+
+        if (interval < MIN_INTERVAL) {
+            throw new IllegalArgumentException("Interval is too small, min: " + MIN_INTERVAL);
+        }
+
+        if (interval > MAX_INTERVAL) {
+            throw new IllegalArgumentException("Interval is too large, max: " + MAX_INTERVAL);
         }
     }
 }
--- src/test/java/com/auo/juppy/runner/RunnerConfigTest.java
+++ src/test/java/com/auo/juppy/runner/RunnerConfigTest.java
@@ -11,34 +11,40 @@ public class RunnerConfigTest {
 
     @Test
     public void testValid() {
-        RunnerConfig config = RunnerConfig.create(URI.create("http://example.com"), 1000, 1000, UUID.randomUUID(), -1);
+        RunnerConfig config = RunnerConfig.create(URI.create("http://example.com"), 1000, 60_000, UUID.randomUUID(), -1);
         config.isValid();
 
-        config = RunnerConfig.create(URI.create("https://www.example.com"), 1000, 1000, UUID.randomUUID(), -1);
+        config = RunnerConfig.create(URI.create("https://www.example.com"), 1000, 60_000, UUID.randomUUID(), -1);
         config.isValid();
     }
 
     @Test
     public void testInvalidHost() {
-        RunnerConfig config = RunnerConfig.create(URI.create("https://.com"), 1000, 1000, UUID.randomUUID(), -1);
+        RunnerConfig config = RunnerConfig.create(URI.create("https://.com"), 1000, 60_000, UUID.randomUUID(), -1);
         assertThrows(IllegalArgumentException.class, config::isValid);
     }
 
     @Test
     public void testInvalidScheme() {
-        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), 1000, 1000, UUID.randomUUID(), -1);
+        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), 1000, 60_000, UUID.randomUUID(), -1);
         assertThrows(IllegalArgumentException.class, config::isValid);
     }
 
     @Test
     public void testInvalidTimeout() {
-        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), -1, 1000, UUID.randomUUID(), -1);
+        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), RunnerConfig.MIN_TIMEOUT - 1, 1000, UUID.randomUUID(), -1);
+        assertThrows(IllegalArgumentException.class, config::isValid);
+
+        config = RunnerConfig.create(URI.create("file://example.com"), RunnerConfig.MAX_TIMEOUT + 1, 1000, UUID.randomUUID(), -1);
         assertThrows(IllegalArgumentException.class, config::isValid);
     }
 
     @Test
     public void testInvalidInterval() {
-        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), 1000, -1, UUID.randomUUID(), -1);
+        RunnerConfig config = RunnerConfig.create(URI.create("file://example.com"), 1000, RunnerConfig.MIN_INTERVAL - 1, UUID.randomUUID(), -1);
+        assertThrows(IllegalArgumentException.class, config::isValid);
+
+        config = RunnerConfig.create(URI.create("file://example.com"), 1000, RunnerConfig.MAX_INTERVAL + 1, UUID.randomUUID(), -1);
         assertThrows(IllegalArgumentException.class, config::isValid);
     }
 
